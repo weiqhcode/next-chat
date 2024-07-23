a34a104e8b9c20c9d44d207a5741bb011d31704f
weiqhcode <weiqh2000@163.com>
2024-07-23 22:57:55 +0800

    取消支持claude图文识别

--- app/utils.ts
+++ app/utils.ts
@@ -253,9 +253,7 @@ export function isVisionModel(model: string) {
   const visionKeywords = [
     "gpt-4-s-read-image",
     "gpt-4-o-read-image",
-    "gpt-4-mobile-read-image",
-    "claude-3-opus-20240229",
-    "claude-3-sonnet-20240229"
+    "gpt-4-mobile-read-image"
   ];
   const isGpt4Turbo =
     model.includes("gpt-4-turbo") && !model.includes("preview");
